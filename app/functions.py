068f11d9625a82cdee2b032cb30485475b764332
shmoki_molla <osolodkov2325@gmail.com>
2024-06-23 15:24:33 +0300

    simpler input

--- app/functions.py
+++ app/functions.py
@@ -1,5 +1,30 @@
 from app.database.models import User
 
+months = {
+    "января": "01",
+    "февраля": "02",
+    "марта": "03",
+    "апреля": "04",
+    "мая": "05",
+    "июня": "06",
+    "июля": "07",
+    "августа": "08",
+    "сентября": "09",
+    "октября": "10",
+    "ноября": "11",
+    "декабря": "12"
+}
+
+year = "2024"
+
+def convert_date(input_string):
+    parts = input_string.split()
+    date = parts[0]
+    month = parts[1].lower()
+
+    if month in months:
+        month_number = months[month]
+        return f"{year}:{month_number}:{date}"
 
 
 
--- app/handlers.py
+++ app/handlers.py
@@ -8,6 +8,7 @@ import app.database.requests as rq
 from app.database.models import User, Case
 from datetime import datetime
 from app.classes import Case, Number, Number1, Day
+from app.functions import convert_date
 
 router = Router()
 
@@ -16,32 +17,36 @@ router = Router()
 @router.message(CommandStart())
 async def start(message: Message):
     await rq.set_user(message.from_user.id, message.from_user.full_name)
-    await message.answer('хз привет', reply_markup=kb.main)
+    await message.answer(f'Привет, {message.from_user.username}', reply_markup=kb.main)
 
 @router.message(Command('help'))
 async def help(message: Message):
-    await message.answer('Сам себе помогай')
+    await message.answer(f'Это Бот-Ежедневник!\nС помощью кнопок на появившейся клавиатуре ты можешь управлять ботом)\n'
+                         f'Главное вводи данные в том формате, в каком он просит!')
 
 @router.message((F.text == 'Записать дело') or (Command('case')))
 async def input(message: Message, state: FSMContext):
     await state.set_state(Case.date)
-    await message.answer('Введите дату в формате "ГГГГ:ММ:ДД"')
+    await message.answer('Введите дату в формате "23 февраля"')
 @router.message(Case.date)
 async def input_date(message: Message, state: FSMContext):
-    await state.update_data(date=message.text)
+    await state.update_data(date=convert_date(message.text))
     await state.set_state(Case.time)
-    await message.answer('Введите время в формате "ЧЧ:ММ:СС"')
+    await message.answer('Введите время в формате "15:30"')
 @router.message(Case.time)
 async def input_time(message: Message, state: FSMContext):
-    await state.update_data(time=message.text)
+    await state.update_data(time=f'{message.text}:00')
     await state.set_state(Case.case)
     await message.answer('Введите запланированное дело')
 @router.message(Case.case)
 async def input_case(message: Message, state: FSMContext):
-    await state.update_data(case=message.text)
-    data = await state.get_data()
-    await rq.set_case(message.from_user.id, datetime.strptime(data["date"], '%Y:%m:%d').date(), datetime.strptime(data["time"], '%H:%M:%S'), data["case"])
-    await message.answer(f'Дело успешно записано!\n{data["date"]}\n{data["time"]}\n{data["case"]}')
+    try:
+        await state.update_data(case=message.text)
+        data = await state.get_data()
+        await rq.set_case(message.from_user.id, datetime.strptime(data["date"], '%Y:%m:%d').date(), datetime.strptime(data["time"], '%H:%M:%S'), data["case"])
+        await message.answer(f'Дело успешно записано!\n{data["date"]}\n{data["time"]}\n{data["case"]}')
+    except Exception:
+        await message.answer('Некорректный ввод')
     await state.clear()
 @router.message(F.text == 'Список дел' or Command('get_todays'))
 async def day_choice(message: Message):
@@ -54,14 +59,17 @@ async def get_todays(callback: CallbackQuery):
 @router.callback_query(F.data == 'another')
 async def get_today(callback: CallbackQuery, state: FSMContext):
     await state.set_state(Day.date)
-    await callback.message.answer('Введите дату в формате "ГГГГ:ММ:ДД"')
+    await callback.message.answer('Введите дату в формате "23 февраля"')
 
 @router.message(Day.date)
 async def set_day(message: Message, state: FSMContext):
-    await state.update_data(date=message.text)
-    data = await state.get_data()
-    today = await rq.get_plans(message.from_user.id, datetime.strptime(data["date"], '%Y:%m:%d').date())
-    await message.answer(f'Ваш список дел на {data["date"]}:\n{today}', reply_markup= await kb.change_list())
+    try:
+        await state.update_data(date=convert_date(message.text))
+        data = await state.get_data()
+        today = await rq.get_plans(message.from_user.id, datetime.strptime(data["date"], '%Y:%m:%d').date())
+        await message.answer(f'Ваш список дел на {data["date"]}:\n{today}', reply_markup= await kb.change_list())
+    except Exception:
+        await message.answer('Некорректный ввод')
     await state.clear()
 
 @router.callback_query(F.data == 'delete')
@@ -72,10 +80,13 @@ async def delete1(callback: CallbackQuery, state: FSMContext):
 
 @router.message(Number.number)
 async def delete2(message: Message, state: FSMContext):
-    await state.update_data(number=message.text)
-    data = await state.get_data()
-    success = await rq.delete_case(message.from_user.id, data["number"])
-    await message.answer(success)
+    try:
+        await state.update_data(number=message.text)
+        data = await state.get_data()
+        success = await rq.delete_case(message.from_user.id, data["number"])
+        await message.answer(success)
+    except Exception:
+        await message.answer('Некорректный ввод')
     await state.clear()
 
 @router.callback_query(F.data == 'execute')
@@ -85,10 +96,13 @@ async def execute1(callback: CallbackQuery, state: FSMContext):
 
 @router.message(Number1.number)
 async def execute2(message: Message, state: FSMContext):
-    await state.update_data(number=message.text)
-    data = await state.get_data()
-    success = await rq.execute_case(message.from_user.id, data["number"])
-    await message.answer(success)
+    try:
+        await state.update_data(number=message.text)
+        data = await state.get_data()
+        success = await rq.execute_case(message.from_user.id, data["number"])
+        await message.answer(success)
+    except Exception:
+        await message.answer('Некорректный ввод')
     await state.clear()
 
 @router.message(F.text == 'Мой прогресс')
